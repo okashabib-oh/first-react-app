f200ae9aacdcbf6068e2a61517b89a1922016cef
Okasha Habib <okasha.qavitech786@gmail.com>
2024-08-09 00:54:22 +0500

    react-start

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '../public/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 id='name'>Okasha Habib</h1>
+      
+      <img src={reactLogo} alt="App Logo" />
+
+      <img src={viteLogo} alt="App Logo" />
+
+    </>
+  )
+}
+
+export default App
